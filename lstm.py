272382b33126480854eddb5022e98fde14179815
rgoyal61 <rigvedgoyal@gatech.edu>
2023-11-30 19:00:25 -0500

    Fix missing import

--- lstm.py
+++ lstm.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
